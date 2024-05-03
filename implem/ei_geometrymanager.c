9bfb4fdc0820a1bf68aacee3b35b0ffacf5a7f49
boony <yi-jun.boon@grenoble-inp.org>
2024-05-03 18:00:15 +0200

    ei_widgetclass_from_name

---
+++ implem/ei_geometrymanager.c
@@ -0,0 +1,37 @@
+#include "ei_geometrymanager.h"
+
+void			ei_geometry_run_finalize(ei_widget_t widget, ei_rect_t* new_screen_location){
+
+}
+
+void			ei_geometrymanager_register	(ei_geometrymanager_t* geometrymanager){
+
+}
+
+ei_geometrymanager_t*	ei_geometrymanager_from_name	(ei_geometrymanager_name_t name){
+
+}
+
+void			ei_geometrymanager_unmap	(ei_widget_t widget){
+
+}
+
+ei_geometrymanager_t*	ei_widget_get_geom_manager	(ei_widget_t widget){
+
+}
+
+
+void			ei_widget_set_geom_manager	(ei_widget_t widget, ei_geometrymanager_t* manager){
+
+}
+
+ei_geom_param_t		ei_widget_get_geom_params	(ei_widget_t widget){
+
+}
+
+
+void			ei_widget_set_geom_params	(ei_widget_t widget, ei_geom_param_t geom_param){
+
+}
+
+
