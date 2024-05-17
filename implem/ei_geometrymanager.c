afa732e484e49cf774ebf7de8ac62245cc4b0d51
mohdnoon <Nur-Hakimi.Mohd-Noor@grenoble-inp.org>
2024-05-17 15:09:38 +0200

    fenetre depassement fix

--- implem/ei_geometrymanager.c
+++ implem/ei_geometrymanager.c
@@ -1,6 +1,7 @@
 #include "ei_geometrymanager.h"
 #include "ei_implementation.h"
 #include "ei_global.h"
+#include "ei_toplevel.h"
 
 static ei_geometrymanager_t** geo_mgr_list = NULL;
 static size_t geo_mgr_list_size = 0;
@@ -13,11 +14,19 @@ void	ei_geometry_run_finalize(ei_widget_t widget, ei_rect_t* new_screen_location
                 return;
         }
 
+        int text_width = 0;
+        int text_height = 0;
+
+        if(strcmp(widget->wclass->name,"toplevel") == 0){
+                toplevel_t* toplevel = (toplevel_t*) widget;
+                hw_text_compute_size(toplevel->title,toplevel->title_font,&text_width,&text_height);
+        }
+
         if (new_screen_location->top_left.x < 0) {
                 new_screen_location->top_left.x = 0;
         }
-        if (new_screen_location->top_left.y < 0) {
-                new_screen_location->top_left.y = 0;
+        if (new_screen_location->top_left.y - text_height < 0) {
+                new_screen_location->top_left.y = 0 + text_height;
         }
         if (new_screen_location->top_left.x + new_screen_location->size.width > root_size->width) {
                 new_screen_location->top_left.x = root_size->width - new_screen_location->size.width;
