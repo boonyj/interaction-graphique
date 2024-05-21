7b2c0f520e7f955dfe46b954736ee233ce06b1ec
boony <yijun.boon@grenoble-inp.org>
2024-05-22 00:18:42 +0200

    application refactoring

--- implem/ei_application.c
+++ implem/ei_application.c
@@ -1,14 +1,13 @@
 #include "ei_application.h"
-#include "ei_implementation.h"
-#include "ei_frame.h"
-#include "ei_placeur.h"
-#include "ei_button.h"
-#include "ei_toplevel.h"
-#include "ei_event.h"
+#include "ei_app_run_tool.h"
 #include "ei_global.h"
-#include "ei_callback_toplevel.h"
-#include "ei_entry_class.h"
+#include "ei_button.h"
+#include "ei_frame.h"
+#include "ei_toplevel.h"
+#include "ei_entry.h"
+#include "ei_placeur.h"
 #include "ei_callback_button.h"
+#include "ei_callback_toplevel.h"
 
 void ei_app_create(ei_size_t main_window_size, bool fullscreen){
         // Initialisation of the application
@@ -39,134 +38,6 @@ void ei_app_create(ei_size_t main_window_size, bool fullscreen){
         root = ei_widget_create("frame", NULL, NULL, NULL);
 }
 
-// Function free all binded events in the events list
-void free_event_list() {
-        ei_linked_event_t* current = linked_event_list;
-        ei_linked_event_t* next_node;
-
-        while (current != NULL) {
-                next_node = current->next;
-                free(current);
-                current = next_node;
-        }
-        linked_event_list = NULL;
-}
-
-void ei_app_free(void){
-        free_event_list();
-        hw_surface_unlock(main_surface);
-        hw_surface_unlock(pick_surface);
-        hw_surface_free(main_surface);
-        hw_surface_free(pick_surface);
-        free(root_size);
-        ei_widget_destroy(root);
-        hw_quit();
-}
-
-// Function to clear the invalidated rectangles list (for cleanup purposes)
-void clear_invalidated_rects() {
-        ei_linked_rect_t* current = invalidated_rects_head;
-        while (current != NULL) {
-                ei_linked_rect_t* next = current->next;
-                free(current);
-                current = next;
-        }
-        invalidated_rects_head = NULL;
-}
-
-void exit_toplevel_button_press(ei_widget_t widget, ei_linked_event_t *head, ei_event_t *event, bool *exit_button_handled) {
-        if (widget->parent != NULL && strcmp(widget->parent->wclass->name, "toplevel") == 0) {
-                if (widget->pick_id == widget->parent->pick_id + 1) {
-                        if ((*event).type == ei_ev_mouse_buttonup) {
-                                while (head != NULL) {
-                                        if (head->eventtype == ei_ev_mouse_buttonup) {
-                                                if (head->widget == widget) {
-                                                        head->callback(widget, event, widget->user_data);
-                                                        (*exit_button_handled) = true;
-                                                        ei_unbind(ei_ev_mouse_buttonup, NULL, "all", callback_buttonup_reverse_relief, widget);
-                                                        //ei_unbind(ei_ev_mouse_buttonup, NULL, "all", callback_buttonup_reverse_relief, widget->next_sibling);
-                                                        break;
-                                                }
-                                        }
-                                        head = head->next;
-                                }
-                        } else if ((*event).type == ei_ev_mouse_buttondown) {
-                                while (head != NULL) {
-                                        if (head->eventtype == ei_ev_mouse_buttondown) {
-                                                if (head->widget == widget) {
-                                                        head->callback(widget, event, widget->user_data);
-                                                        break;
-                                                }
-                                        }
-                                        head = head->next;
-                                }
-                        }
-                }
-        }
-}
-
-void draw_all_widgets(ei_rect_t *clipper) {
-        if (root->wclass != NULL) {
-                if (root->wclass->drawfunc != NULL) {
-                        root->wclass->drawfunc(root, main_surface, pick_surface, NULL);
-                }
-        }
-        clipper = &(root->children_head->screen_location);
-        ei_impl_widget_draw_children(root, main_surface, pick_surface, clipper);
-}
-
-void get_event(ei_rect_t *clipper, ei_event_t *event, struct ei_impl_widget_t *widget, ei_linked_event_t *head) {
-        head = linked_event_list;
-        //Search for event in list
-        while (head != NULL) {
-                if (head->eventtype == (*event).type) {
-                        if(head->widget != NULL){
-                                if(widget == head->widget){
-                                        head->callback(widget, event, widget->user_data);
-                                        draw_all_widgets(clipper);
-                                }
-                        } else {
-                                if(strcmp(head->tag, widget->wclass->name) == 0 ||
-                                   strcmp(head->tag, "all") == 0){
-                                        head->callback(widget, event, head->user_param);
-                                }
-                        }
-                }
-                head = head->next;
-        }
-}
-
-void get_keydown_event(struct ei_impl_widget_t *widget, ei_linked_event_t *head, ei_rect_t **clipper, ei_event_t *event) {
-        while (head != NULL) {
-                if (head->eventtype == (*event).type) {
-                        head->callback(widget, event, head->user_param);
-                        if (root->wclass != NULL) {
-                                if (root->wclass->drawfunc != NULL) {
-                                        root->wclass->drawfunc(root, main_surface, pick_surface, NULL);
-                                }
-                        }
-                        (*clipper) = &(root->children_head->screen_location);
-                        ei_impl_widget_draw_children(root, main_surface, pick_surface, (*clipper));
-                }
-                head = head->next;
-        }
-}
-
-void run_ev_app_event(struct ei_impl_widget_t *widget,ei_linked_event_t *head, ei_rect_t **clipper, ei_event_t *event) {
-        while (head != NULL) {
-                if (head->eventtype == ei_ev_app) {
-                        head->callback(NULL, event, head->user_param);
-                        if (root->wclass != NULL) {
-                                if (root->wclass->drawfunc != NULL) {
-                                        root->wclass->drawfunc(root, main_surface, pick_surface, NULL);
-                                }
-                        }
-                        (*clipper) = &(root->children_head->screen_location);
-                        ei_impl_widget_draw_children(root, main_surface, pick_surface, (*clipper));
-                }
-                head = head->next;
-        }
-}
 
 void ei_app_run(void) {
         // Get the root widget of the application
@@ -203,7 +74,7 @@ void ei_app_run(void) {
                 hw_surface_update_rects(main_surface, NULL);
                 //hw_surface_update_rects(pick_surface, NULL);
 
-                clear_invalidated_rects();
+                //clear_invalidated_rects();
                 hw_surface_lock(main_surface);
                 hw_surface_lock(pick_surface);
 
@@ -266,6 +137,18 @@ void ei_app_invalidate_rect(const ei_rect_t* rect) {
                 }
                 current->next = new_node;
         }
+}
+
+
+void ei_app_free(void){
+        free_event_list();
+        hw_surface_unlock(main_surface);
+        hw_surface_unlock(pick_surface);
+        hw_surface_free(main_surface);
+        hw_surface_free(pick_surface);
+        free(root_size);
+        ei_widget_destroy(root);
+        hw_quit();
 }
 
 void ei_app_quit_request(void){
