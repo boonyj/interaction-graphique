a453e7ef7b8a002f4ac38958a8e6ee01b7302747
boony <yijun.boon@grenoble-inp.org>
2024-05-22 00:22:36 +0200

    fichier app_run_tool

---
+++ implem/ei_app_run_tool.c
@@ -0,0 +1,119 @@
+#include "ei_app_run_tool.h"
+
+void exit_toplevel_button_press(ei_widget_t widget, ei_linked_event_t *head, ei_event_t *event, bool *exit_button_handled) {
+        if (widget->parent != NULL && strcmp(widget->parent->wclass->name, "toplevel") == 0) {
+                if (widget->pick_id == widget->parent->pick_id + 1) {
+                        if ((*event).type == ei_ev_mouse_buttonup) {
+                                while (head != NULL) {
+                                        if (head->eventtype == ei_ev_mouse_buttonup) {
+                                                if (head->widget == widget) {
+                                                        head->callback(widget, event, widget->user_data);
+                                                        (*exit_button_handled) = true;
+                                                        ei_unbind(ei_ev_mouse_buttonup, NULL, "all", callback_buttonup_reverse_relief, widget);
+                                                        //ei_unbind(ei_ev_mouse_buttonup, NULL, "all", callback_buttonup_reverse_relief, widget->next_sibling);
+                                                        break;
+                                                }
+                                        }
+                                        head = head->next;
+                                }
+                        } else if ((*event).type == ei_ev_mouse_buttondown) {
+                                while (head != NULL) {
+                                        if (head->eventtype == ei_ev_mouse_buttondown) {
+                                                if (head->widget == widget) {
+                                                        head->callback(widget, event, widget->user_data);
+                                                        break;
+                                                }
+                                        }
+                                        head = head->next;
+                                }
+                        }
+                }
+        }
+}
+
+void draw_all_widgets(ei_rect_t *clipper) {
+        if (root->wclass != NULL) {
+                if (root->wclass->drawfunc != NULL) {
+                        root->wclass->drawfunc(root, main_surface, pick_surface, NULL);
+                }
+        }
+        clipper = &(root->children_head->screen_location);
+        ei_impl_widget_draw_children(root, main_surface, pick_surface, clipper);
+}
+
+void get_event(ei_rect_t *clipper, ei_event_t *event, struct ei_impl_widget_t *widget, ei_linked_event_t *head) {
+        head = linked_event_list;
+        //Search for event in list
+        while (head != NULL) {
+                if (head->eventtype == (*event).type) {
+                        if(head->widget != NULL){
+                                if(widget == head->widget){
+                                        head->callback(widget, event, widget->user_data);
+                                        draw_all_widgets(clipper);
+                                }
+                        } else {
+                                if(strcmp(head->tag, widget->wclass->name) == 0 ||
+                                   strcmp(head->tag, "all") == 0){
+                                        head->callback(widget, event, head->user_param);
+                                }
+                        }
+                }
+                head = head->next;
+        }
+}
+
+void get_keydown_event(struct ei_impl_widget_t *widget, ei_linked_event_t *head, ei_rect_t **clipper, ei_event_t *event) {
+        while (head != NULL) {
+                if (head->eventtype == (*event).type) {
+                        head->callback(widget, event, head->user_param);
+                        if (root->wclass != NULL) {
+                                if (root->wclass->drawfunc != NULL) {
+                                        root->wclass->drawfunc(root, main_surface, pick_surface, NULL);
+                                }
+                        }
+                        (*clipper) = &(root->children_head->screen_location);
+                        ei_impl_widget_draw_children(root, main_surface, pick_surface, (*clipper));
+                }
+                head = head->next;
+        }
+}
+
+void run_ev_app_event(struct ei_impl_widget_t *widget,ei_linked_event_t *head, ei_rect_t **clipper, ei_event_t *event) {
+        while (head != NULL) {
+                if (head->eventtype == ei_ev_app) {
+                        head->callback(NULL, event, head->user_param);
+                        if (root->wclass != NULL) {
+                                if (root->wclass->drawfunc != NULL) {
+                                        root->wclass->drawfunc(root, main_surface, pick_surface, NULL);
+                                }
+                        }
+                        (*clipper) = &(root->children_head->screen_location);
+                        ei_impl_widget_draw_children(root, main_surface, pick_surface, (*clipper));
+                }
+                head = head->next;
+        }
+}
+
+// Function free all binded events in the events list
+void free_event_list() {
+        ei_linked_event_t* current = linked_event_list;
+        ei_linked_event_t* next_node;
+
+        while (current != NULL) {
+                next_node = current->next;
+                free(current);
+                current = next_node;
+        }
+        linked_event_list = NULL;
+}
+
+// Function to clear the invalidated rectangles list (for cleanup purposes)
+void clear_invalidated_rects() {
+        ei_linked_rect_t* current = invalidated_rects_head;
+        while (current != NULL) {
+                ei_linked_rect_t* next = current->next;
+                free(current);
+                current = next;
+        }
+        invalidated_rects_head = NULL;
+}
---
+++ implem/ei_app_run_tool.h
@@ -0,0 +1,66 @@
+#ifndef PROJETC_IG_EI_APP_RUN_TOOL_H
+#define PROJETC_IG_EI_APP_RUN_TOOL_H
+
+#include "ei_implementation.h"
+#include "ei_global.h"
+#include "ei_callback_button.h"
+
+/**
+ * @brief   Handles button press events specifically for toplevel exit buttons.
+ *
+ * @param   widget              The widget that received the event.
+ * @param   head                The head of the linked list of events.
+ * @param   event               The event containing all its parameters (type, etc.).
+ * @param   exit_button_handled A boolean flag to indicate if the exit button event was handled.
+ */
+void exit_toplevel_button_press(ei_widget_t widget, ei_linked_event_t *head, ei_event_t *event, bool *exit_button_handled);
+
+/**
+ * @brief   Draws all widgets in the application.
+ *
+ * @param   clipper             The clipping rectangle to restrict drawing.
+ */
+void draw_all_widgets(ei_rect_t *clipper);
+
+/**
+ * @brief   Handles generic events for widgets.
+ *
+ * @param   clipper             The clipping rectangle to restrict drawing.
+ * @param   event               The event containing all its parameters (type, etc.).
+ * @param   widget              The widget that received the event.
+ * @param   head                The head of the linked list of events.
+ */
+void get_event(ei_rect_t *clipper, ei_event_t *event, struct ei_impl_widget_t *widget, ei_linked_event_t *head);
+
+/**
+ * @brief   Handles keydown events specifically for widgets.
+ *
+ * @param   widget              The widget that received the event.
+ * @param   head                The head of the linked list of events.
+ * @param   clipper             The clipping rectangle to restrict drawing.
+ * @param   event               The event containing all its parameters (type, etc.).
+ */
+void get_keydown_event(ei_impl_widget_t *widget, ei_linked_event_t *head, ei_rect_t **clipper, ei_event_t *event);
+
+/**
+ * @brief   Handles application-specific events.
+ *
+ * @param   widget              The widget that received the event.
+ * @param   head                The head of the linked list of events.
+ * @param   clipper             The clipping rectangle to restrict drawing.
+ * @param   event               The event containing all its parameters (type, etc.).
+ */
+void run_ev_app_event(ei_impl_widget_t *widget, ei_linked_event_t *head, ei_rect_t **clipper, ei_event_t *event);
+
+/**
+ * @brief   Frees all bound events in the event list.
+ */
+void free_event_list();
+
+/**
+ * @brief   Clears the list of invalidated rectangles.
+ */
+void clear_invalidated_rects();
+
+
+#endif //PROJETC_IG_EI_APP_RUN_TOOL_H
